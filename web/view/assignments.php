9de0848bcc354f3da8d7db91b38e867a83f78bd7
gongorafrancisco <59567236+gongorafrancisco@users.noreply.github.com>
2020-05-01 15:43:51 -0500

    Assignmnets page added

---
+++ web/view/assignments.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8">
+        <title>Assignments</title>
+         <?php include $_SERVER['DOCUMENT_ROOT'].'common/head.php';?>
+    </head>
+    <body class="text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="../">Home</a>
+      </nav>
+    </div>
+  </header>
+
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Coming soon</h1>
+  </main>
+</div>
+        <script src="js/jquery-3.5.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        </body>
+</html>
