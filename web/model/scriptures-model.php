9eae9dab3fb673d8530faefe944d391763876991
gongorafrancisco <59567236+gongorafrancisco@users.noreply.github.com>
2020-05-20 18:24:14 -0500

    test 9

--- web/model/scriptures-model.php
+++ web/model/scriptures-model.php
@@ -1,6 +1,7 @@
 <?php
 // Connect to Heroku Databse
-  try
+function herokuConnect(){
+try
 {
   $dbUrl = getenv('DATABASE_URL');
 
@@ -15,15 +16,17 @@
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $db;
 }
 catch (PDOException $ex)
 {
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
-
+}  
 // Make a simple query
 function getScriptures(){
+  $db = herokuConnect();
   $sql = 'SELECT scripture_book, scripture_chapter, scripture_verse, scripture_content FROM scriptures';
   $stmt = $db->prepare($sql);
   $stmt->execute();
