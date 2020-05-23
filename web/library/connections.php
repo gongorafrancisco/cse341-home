65ce2b207ab83e565546dd010347ced1e75da6b6
gongorafrancisco <59567236+gongorafrancisco@users.noreply.github.com>
2020-05-22 23:38:54 -0500

    Checking db connection 1

--- web/library/connections.php
+++ web/library/connections.php
@@ -4,7 +4,7 @@ function herokuConnect()
 {
   $db = NULL;
   try {
-    $dbUrl = getenv('DATABASE_URL');              
+    $dbUrl = getenv('postgres://fixtsckmcihgws:e74a380b7061d9729e140a5fa67a5ac26c9f6a2c82f6b0df5ec34460de566ef4@ec2-54-175-117-212.compute-1.amazonaws.com:5432/d1ue3boa7vgphf');              
     $dbOpts = parse_url($dbUrl);
 
     $dbHost = $dbOpts["host"];
