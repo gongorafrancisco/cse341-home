aa6a3d79fa7705d9fd2ec687b87f2bfc18e288ca
gongorafrancisco <59567236+gongorafrancisco@users.noreply.github.com>
2020-05-23 21:09:40 -0500

    test 31

--- web/common/sf-customers-form.php
+++ web/common/sf-customers-form.php
@@ -1,15 +1,17 @@
 <form class="needs-validation" action="../sf-customers/index.php" method="post">
                     <div class="form-row align-items-center">
+                    <div class="col-auto">
+                            <label>Search</label>
+                        </div>
                         <div class="col-auto">
                             <div class="input-group mb-2">
-                                <input type="text" class="form-control" id="inlineFormInputGroup" name="filter_value" placeholder="Write something" required>
-                                <small id="filterHelp" class="form-text text-muted">The search box is case sensitive, meaning that A is not the same than a.</small>
+                                <input type="text" class="form-control" id="inlineFormInputGroup" name="filter_value" placeholder="(e.g. Amazon)" required>
                             </div>
                         </div>
-
                         <div class="col-auto">
                             <button type="submit" class="btn btn-primary mb-2">Apply</button>
                             <input type="hidden" name="action" value="filterCustomers">
                         </div>
                     </div>
                 </form>
+                <small>This search box is case sentive, meaning that 'A' will not be treated as 'a' and viceversa.</small>
