721e711660c688dc22592edec90ec8d00c3a79f8
gongorafrancisco <59567236+gongorafrancisco@users.noreply.github.com>
2020-06-06 19:49:05 -0500

    Adding instrucctions messages to dashboard

--- web/view/sf-dashboard.php
+++ web/view/sf-dashboard.php
@@ -1,10 +1,10 @@
-<?php 
-if (!isset($_SESSION['member_name'])) { 
-        header("Location: /salesfu");
-    	die();
-    }
+<?php
+if (!isset($_SESSION['member_name'])) {
+    header("Location: /salesfu");
+    die();
+}
 $pageName = "Dashboard";
-?> 
+?>
 <!doctype html>
 <html lang="en-US">
 
@@ -15,12 +15,54 @@ $pageName = "Dashboard";
 </head>
 
 <body>
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/common/sf-navt.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/sf-navt.php'; ?>
     <div class="container-fluid h-100">
         <div class="row h-100">
-        <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/sf-navl.php'; ?>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/sf-navl.php'; ?>
             <div class="col h-100 py-4 px-3">
-
+                <div class="col mb-5">
+                    <h5 class="text-info">Process to create a request for a new Company</h5>
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item">Add a Customer -></li>
+                        <li class="list-group-item">Fill the form fields</li>
+                        <li class="list-group-item">Add a Contact -></li>
+                        <li class="list-group-item">Fill the form fields</li>
+                        <li class="list-group-item">Add a Request -></li>
+                        <li class="list-group-item">Fill the form fields</li>
+                        <li class="list-group-item">End</li>
+                    </ul>
+                </div>
+                <div class="col my-5">
+                    <h5 class="text-info">Process to create a request for an existing Company</h5>
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item">Add a Request -></li>
+                        <li class="list-group-item">Fill the form fields</li>
+                        <li class="list-group-item">End</li>
+                    </ul>
+                </div>
+                <div class="col my-5">
+                    <h5 class="text-info">Process to complete a request / create a new quote</h5>
+                    <ul class="list-group list-group-horizontal">
+                        <li class="list-group-item">View all Requests -></li>
+                        <li class="list-group-item">Click on the "Complete" button -></li>
+                        <li class="list-group-item">Fill the all the quote fields</li>
+                        <li class="list-group-item">End</li>
+                    </ul>
+                </div>
+                <div class="col my-5">
+                    <h5 class="text-info">Pending functionality</h5>
+                    <ul class="list-group">
+                        <li class="list-group-item">Display key indicators for not completed requests, completed requests, new requests at this Dashboard page.</li>
+                        <li class="list-group-item">Display all related contacts, requests and quotes on the "Customer Details" page.</li>
+                        <li class="list-group-item">Display a link for "Details" on the Customers Page that goes to a page with all Contact's quote requests and quotes.</li>
+                        <li class="list-group-item">Display a dynamic help message on the search box at the Quote Requests page.</li>
+                        <li class="list-group-item">Display details from the quote request like the actual items request content and the related quote.</li>
+                        <li class="list-group-item">Display date of completition, quote# on the Quote Request Page based on the complet status.</li>
+                        <li class="list-group-item">Display a link for "Details" on the Quotes Page that goes to a page with details like the subtotal, taxes, and actual quoted items.</li>
+                        <li class="list-group-item">Activate functionality for "Modify" and "Delete" buttons at the Quotes Page.</li>
+                        <li class="list-group-item">Activate functionality for filter quotes.</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
